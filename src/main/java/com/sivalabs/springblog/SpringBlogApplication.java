f009300d1306ecbd2b30775ffdd999827fbc7c0d
Geovanny Mendoza <mgeovanny@uninorte.edu.co>
2024-11-16 01:09:49 -0500

    V[0.0.2] creation entitie, test of repo and add spotless

--- src/main/java/com/sivalabs/springblog/SpringBlogApplication.java
+++ src/main/java/com/sivalabs/springblog/SpringBlogApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBlogApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogApplication.class, args);
+    }
 }
---
+++ src/test/resources/test-data.sql
@@ -0,0 +1,6 @@
+truncate table users CASCADE;
+
+INSERT INTO USERS (email, password, name, role)
+VALUES
+    ('siva.prasad@example.com', 'password123', 'Siva Prasad', 'Admin'),
+    ('geovanny.mendoza@example.com', 'password456', 'Geovanny Mendoza', 'User');
