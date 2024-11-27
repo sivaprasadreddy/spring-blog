c3845e8256863541849abaa889385d616b82b362
Geovanny Mendoza <mgeovanny@uninorte.edu.co>
2024-11-26 21:24:38 -0500

    V[0.0.5] update controller

--- src/main/java/com/sivalabs/springblog/post/web/controllers/PostController.java
+++ src/main/java/com/sivalabs/springblog/post/web/controllers/PostController.java
@@ -5,13 +5,13 @@ import com.sivalabs.springblog.post.domain.Post;
 import com.sivalabs.springblog.post.domain.PostService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/posts")
 public class PostController {
     private static final Logger log = LoggerFactory.getLogger(PostController.class);
